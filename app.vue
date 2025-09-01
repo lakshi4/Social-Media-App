dfb41a57fead8c467e689eef50e2dedbd31060b5
lakshi4 <148441415+lakshi4@users.noreply.github.com>
2025-09-01 13:31:12 +0530

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{'dark': darkMode}">
+    <div class="bg-white dark:bg-dim-900">
+   
+    <div class ="min-h-full">
+
+      <div class ="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+        <!-- Left Sidebar   -->
+
+        <div class ="hidden md:block xs-col-span-1 xl-col-span-2">
+          <div class ="sticky top-0">
+            <SidebarLeft/>
+          </div>
+          
+        </div>
+
+        <!-- Main content -->
+
+        <!-- Right Sidebar-->
+        </div>  
+
+    
+
+    </div>
+    </div>
+
+  </div>
+</template>
+
+
+<script setup>
+//import SidebarLeft from '~/components/Sidebar/Left.vue' // Use this if your file is Left.vue
+const darkMode = ref(false)
+
+
+</script>
+<style>
+.logo {
+  height: 10rem; /* h-10 */
+  width: 10rem;  /* w-10 */
+  border-radius: 9999px; /* rounded-full */
+  color: #fff; /* dark:text-white fallback */
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-06-01',
+  devtools: { enabled: true },  
+
+  modules: ['@nuxtjs/tailwindcss']
+
+
+})
