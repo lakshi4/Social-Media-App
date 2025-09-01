cee09f5858ec328f566db7d93391d24c4f00395a
lakshi4 <148441415+lakshi4@users.noreply.github.com>
2025-09-01 13:32:05 +0530

    Add files via upload

---
+++ components/Logo/Blink.vue
@@ -0,0 +1,5 @@
+<template>
+    <!-- Use either SVG or IMG, not both nested -->
+    <img src="/images/blinkL.png" class="h-18 w-18 rounded-full dark:text-white" alt="Blink logo" />
+</template>
+
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <nuxt-link to="#" class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 dark:text-white"
+    :class ="defaultTransition">
+        <div class="w-6 h-6 text-dark" >
+            <slot name="icon" ></slot>
+        </div>
+
+        <div class="ml-4 text-lg xl:block" :class="textClasses">
+            <slot name="name" ></slot>
+        </div>
+
+    </nuxt-link>
+
+</template>
+<script setup>
+import useTailwindConfig from '~/composables/useTailwindConfig';
+const {defaultTransition} =useTailwindConfig()
+
+const props = defineProps({
+    active: {
+        type:Boolean,
+        default: false
+    }
+})
+
+const textClasses = computed(() => {
+    return props.active ? ']font-semibold' : 'text-normal';
+})
+
+</script>
